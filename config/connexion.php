eb1dec9938968df7e9fb539d4c57e9ff3d1f0b22
Miriam Grizel <39226761+GrizelGonzalez@users.noreply.github.com>
2021-02-11 22:58:18 -0600

    Check connection

--- config/connexion.php
+++ config/connexion.php
@@ -8,4 +8,8 @@ $db = "almacenes";
 $conexion = mysqli_connect($host,$user,$pass,$db);
 mysqli_set_charset($conexion,'utf8');
 
-?>
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
