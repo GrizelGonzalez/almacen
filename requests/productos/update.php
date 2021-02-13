ca5888e5c5c2123af169db6ed749fe56008568ec
GrizelGonzalez <miriamgonzalez859@gmail.com>
2021-02-13 16:08:16 -0600

    has tienda pending....

--- requests/productos/update.php
+++ requests/productos/update.php
@@ -9,37 +9,22 @@ $stock = $_POST['stock'];
 $categoria = $_POST['categoria'];
 $sucursales = $_POST['sucursales'];
 
-var_dump($sucursales);
-die();
+
+$consulta = "UPDATE producto SET nombre = '$nombre',descripcion  = '$descripcion',precio = '$precio',stock = '$stock',categoria_id = '$categoria' 
+            WHERE id = {$id}";
+
+$consultaDelete = "DELETE FROM producto_has_tienda WHERE producto_id = $id";
 
 
-$consulta = "
-UPDATE producto 
-    SET nombre = '$nombre',
-        descripcion  = '$descripcion',
-        precio = '$precio',
-        stock = '$stock',
-        categoria_id = '$categoria' WHERE id = {$id}   
-";
-
-$consultaHasTienda = "SELECT COUNT(*) AS hasTienda FROM producto_has_tienda WHERE producto_id = $id";
-$resultadoHasTienda = mysqli_query($connexion, $consultaHasTienda);
-$countTienda = mysqli_fetch_assoc($resultadoHasTienda);
-$hasTienda = (int) $countTienda['hasTienda'];
-
-$consultaProductoHasTienda = "INSERT INTO producto_has_tienda (producto_id, tienda_id) VALUES ($id, )";
-
-
-if ($hasTienda > 0) {
-
-}
-die();
-
-
-if (mysqli_query($connexion, $consulta)) {
+if (mysqli_query($connexion, $consulta) && mysqli_query($connexion, $consultaDelete)) {
+    if (count($sucursales) != 0) {
+        for ($i = 0; $i <= count($sucursales); $i++) {
+            mysqli_query($connexion, "INSERT INTO producto_has_tienda (producto_id, tienda_id) VALUES ($id, $sucursales[$i])");
+        }
+    }
     header("Location: ../../views/productos/lista.php");
     $_SESSION['response'] = 'success,Registro actualizado correctamente';
 } else {
     $_SESSION['response'] = "danger," . mysqli_error($connexion);
-    header('Location: ../../views/productos/editar.php?id='.$id);
+    header('Location: ../../views/productos/editar.php?id=' . $id);
 }
